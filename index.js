9f51369c48c2d152139661d468423c4092cabb01
UILcoder <836219401@qq.com>
2021-09-24 16:20:11 +0800

    four

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ arr.forEach(item => {
   item.onclick = styleActive
 
 });
-localStorage.setItem('shopping_car', [])
 let fixHeight = document.body.offsetHeight - 240
 ifm.style.height = fixHeight + 'px'
 function styleActive() {
